d9c4873eeb6bc2a32eda893ae1872170b5eddbbb
frenkiart <frenki_art@mail.ru>
2022-05-18 16:51:13 +0300

    feat: finish task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const fs = require('fs');
+const path = require('path');
+
+const pathToFile = path.join(__dirname, 'text.txt');
+const readableStream = fs.createReadStream(pathToFile, 'utf8');
+
+readableStream.on('data', function (chunk) {
+  console.log(chunk);
+});
